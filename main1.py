a7d245827400867ac3b7da010fb91a2924e44e93
Oleksandr Mukas <oleksandr.mukas@gmail.com>
2025-02-17 15:46:36 +0200

    Add new page "generate"

--- main1.py
+++ main1.py
@@ -1,9 +1,19 @@
+import string
+import random
 import cherrypy
 
 class HelloWorld(object):
+
+    # 127.0.0.1:8080/index
     @cherrypy.expose
     def index(self):
         return "Hello World, Oleksandr!"
+
+    # 127.0.0.1:8080/generate
+    @cherrypy.expose
+    def generate(self):
+        return ''.join(random.sample(string.hexdigits, 8))
+
 
 if __name__ == "__main__":
     cherrypy.quickstart( HelloWorld() )
